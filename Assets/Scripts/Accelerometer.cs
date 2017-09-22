bfc4d110921bb3f23627e800dd6c5650bd8b8dd3
mjackson34 <mjackson34@mail.pima.edu>
2017-09-21 19:21:47 -0700

    Balance Ball scene
    Started working on the Accelerometer script and a ball balancing game.
    
    Still playing around with getting a smooth movement with tilting.

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Accelerometer : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed = 1;
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        if (rb == null) { rb = gameObject.GetComponent<Rigidbody>(); }
+        
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        text.text = (Input.acceleration.x).ToString() + ", " + gameObject.transform.position;
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+        //dir.y = Input.acceleration.y;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        dir *= Time.deltaTime;
+        //rb.AddForce(dir, ForceMode.Acceleration);
+        transform.position += dir;
+        //transform.Translate(dir * speed);
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    public float rotationTime = 2f;
+
+    private float rotationTimeLeft = 0f;
+    private Quaternion target = Quaternion.Euler(0f, 0f, 0f);
+
+    // Update is called once per frame
+    void Update () {
+        rotationTimeLeft += Time.deltaTime;
+
+        if(rotationTimeLeft > rotationTime)
+        {
+            float zTilt = Random.Range(-7.0f, 7.0f);
+            target = Quaternion.Euler(0f, 0f, zTilt);
+            rotationTimeLeft = 0f;
+            
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
+    }
+}
