22376a723837bc5f27c3d78566d70c363bcfd860
ajseward <andrewseward93@gmail.com>
2017-10-12 17:47:13 -0700

    changed dictionary to two arrays

---
+++ Assets/Scripts/Menu/Transition/TransitionInstructions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionInstructions : MonoBehaviour
+{
+    public string[] SceneNames;
+    public Sprite[] InstructionSprites;
+    private Dictionary<string, Sprite> _instructionMap;
+
+    public void Awake()
+    {
+        Debug.Assert(SceneNames.Length == InstructionSprites.Length, "SceneNames.Length and InstructionSprites.Length not equal!");
+
+        _instructionMap = new Dictionary<string, Sprite>();
+        for (int i = 0; i < SceneNames.Length; ++i)
+        {
+            _instructionMap.Add(SceneNames[i], InstructionSprites[i]);
+        }
+    }
+    public void Start()
+    {
+        var img = GetComponent<Image>();
+        if (_instructionMap.ContainsKey(SceneLoader.ActiveLoader.NextSceneName))
+        {
+            img.sprite = _instructionMap[SceneLoader.ActiveLoader.NextSceneName];
+        }
+        else
+        {
+            img.color = Color.clear;
+        }
+    }
+}
